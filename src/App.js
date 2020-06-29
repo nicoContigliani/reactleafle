ce22ff4c6e84add54e33ceac999852f198dbb300
Nicolas_Contigliani <nico.contigliani@gmail.com>
2020-06-29 08:48:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import useSwr from "swr";
 import './App.css';
+const fetcher = (...args) =>
+  fetch(...args)
+    .then(response => response.json());
+const icon = new Icon({
+  iconUrl: "/handcuffs.svg",
+  iconSize: [25, 25]
+});
+export default function App() {
 
-function App() {
+  const url = "https://data.police.uk/api/crimes-street/all-crime?lat=52.629729&lng=-1.131592&date=2019-10";
+  const { data, error } = useSwr(url, fetcher);
+  const crimes = data && !error ? data.slice(0, 100) : [];
+  const [activeCrime, setActiveCrime] = React.useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Map center={[52.6376, -1.135171]} zoom={12}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {crimes.map(crime => <Marker
+        key={crime.id}
+        position={[crime.location.latitude, crime.location.longitude]}
+        //  icon={icon}
+        onclick={() => {
+          setActiveCrime(crime);
+        }}
+      />
+      )}
+      {activeCrime&&<Popup position={[activeCrime.location.latitude, activeCrime.location.longitude]}
+      onClose={()=>{
+        setActiveCrime(null);
+      }}
+      >
+        <div><h2>
+          {activeCrime.category}
+          </h2></div></Popup>}
+    </Map>
+  )
 }
-
-export default App;
